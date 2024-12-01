3c8165d8799bcd72f8d85cadb489028524c7ef99
detruktor37@gmail.com <detruktor37@gmail.com>
2024-12-01 18:34:13 +0700

    weapon

--- тест/тест.cpp
+++ тест/тест.cpp
@@ -1,8 +1,25 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+
+class weapon {
+public:
+    int dmg;
+    int lvl;
+    int lvlOfSkill;
+    virtual void atack() = 0;
+    virtual void upgrade() = 0;
+    string rarity;
+};
+class meele_weapon : public weapon {
+    string HuntingDagger;
+};
+class range_wepon : public weapon {
+    string bow;
+};
+class magic_weapon : public weapon {
+    string soulBlade;
+};
 int main()
 {
-   cout << "Matvey natural\n";
+    cout << "МАТВЕЙ НАТУРАЛ!\n";//dfgdfgdfg
 }
-
