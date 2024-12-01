a05f90e258cc826f9d90e2714d0140b29176d55e
Matvey870 <RussiA2019@>
2024-12-01 18:37:17 +0700

    sd

--- тест/тест.cpp
+++ тест/тест.cpp
@@ -19,7 +19,44 @@ class range_wepon : public weapon {
 class magic_weapon : public weapon {
     string soulBlade;
 };
+
 int main()
 {
     cout << "МАТВЕЙ НАТУРАЛ!\n";//dfgdfgdfg
+	int lvl = 0;
+	int exp = 0;
+	int dexterity = 0;
+	int intelligence = 0;
+	int power = 0;
+	int luck = 0;
+
+	if exp = 100 {
+		cout << "У вас новый уровень";
+		cout << "Ветка прокачки";
+		cout << "1-Прокочать силу";
+		cout << "2-Прокочать интилект";
+		cout << "3-Прокочать ловкость";
+		cout << "4-Прокочать удача";
+
+		int x;
+		switch (x)
+		{
+		case 1:
+			cout << "Вы прокачиваете силу";
+			int power = 0 + 1;
+
+		case 2:
+			cout << "Вы прокочали интелект";
+			int intelligence = 0 + 1;
+
+		case 3:
+			cout << "Вы прокочали ловкость";
+			int dextrity = 0 + 1;
+
+		case 4:
+			cout << "Вы прокочали удачу";
+			int luck = 0 + 1;
+			break;
+		}
+	}
 }
